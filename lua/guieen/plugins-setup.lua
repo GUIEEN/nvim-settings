f9b79623279c6825bb9915b506bd1763cfe33f82
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2022-12-24 20:58:29 +0900

    Added toggleterm

--- lua/guieen/plugins-setup.lua
+++ lua/guieen/plugins-setup.lua
@@ -123,4 +123,12 @@ return packer.startup(function(use)
 	if packer_bootstrap then
 		require("packer").sync()
 	end
+
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
 end)
