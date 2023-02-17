fb8b4cd7bd60cb60efebc82491e4ed9ab7678a3a
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2023-02-17 11:24:47 +0900

    Validate telescope requirements: ripgrep

--- lua/guieen/plugins/telescope.lua
+++ lua/guieen/plugins/telescope.lua
@@ -1,13 +1,22 @@
 -- import telescope plugin safely
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
+	print("[telescope] telescope not installed")
 	return
 end
 
 -- import telescope actions safely
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
+	print("[telescope] telescope.actions not installed")
 	return
+end
+
+-- https://github.com/nvim-telescope/telescope.nvim#suggested-dependencies
+-- BurntSushi/ripgrep is required for live_grep and grep_string and is the first priority for find_files.
+local is_ripgrep_installed = vim.fn.executable("rg")
+if is_ripgrep_installed == 0 then
+	error("[telescope] ripgrep NOT_FOUND. ripgrep is required for live_grep. `brew install ripgrep`", 2)
 end
 
 -- configure telescope
