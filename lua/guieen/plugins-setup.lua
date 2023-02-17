85d1e150f73f61ed0aaa1d64dd8d6cb707e259e7
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2023-02-17 11:25:46 +0900

    Explicitly specify requirements of telescope

--- lua/guieen/plugins-setup.lua
+++ lua/guieen/plugins-setup.lua
@@ -71,7 +71,7 @@ return packer.startup(function(use)
 	-- fuzzy finding w/ telescope
 	-- dependency: brew install ripgrep
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } }) -- fuzzy finder
 
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
